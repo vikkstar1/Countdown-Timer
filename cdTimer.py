8b198f0d598fd18e05b86950c200d73ce64d1eb8
vikkstar1 <bubupostvitalie@gmail.com>
2022-08-02 23:40:39 +0300

    Added the time loop for the timer

---
+++ cdTimer.py
@@ -0,0 +1,12 @@
+import time
+
+def timer(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+        
+t = input("Enter the time in seconds: ")
+timer(int(t))  
